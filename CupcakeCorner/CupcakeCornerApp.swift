9c10207c0fae017ae2d04bb4c97ebddc7627cc61
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-03-18 12:40:52 -0600

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Mitch Andrade on 3/18/22.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
