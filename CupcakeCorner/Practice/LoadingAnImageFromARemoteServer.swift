f66e941c4a59d53d381f505fd529e965f78642dd
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-03-18 15:28:37 -0600

    Second Commit

---
+++ CupcakeCorner/Practice/LoadingAnImageFromARemoteServer.swift
@@ -0,0 +1,20 @@
+//
+//  LoadingAnImageFromARemoteServer.swift
+//  CupcakeCorner
+//
+//  Created by Mitch Andrade on 3/18/22.
+//
+
+import SwiftUI
+
+struct LoadingAnImageFromARemoteServer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoadingAnImageFromARemoteServer_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingAnImageFromARemoteServer()
+    }
+}
