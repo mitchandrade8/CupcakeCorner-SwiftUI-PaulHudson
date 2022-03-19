944970593b13aa0a386c16a80ada2901aae293f8
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-03-18 21:27:00 -0600

    Third Commit

---
+++ CupcakeCorner/Practice/ValidatingAndDisablingForms.swift
@@ -0,0 +1,20 @@
+//
+//  ValidatingAndDisablingForms.swift
+//  CupcakeCorner
+//
+//  Created by Mitch Andrade on 3/18/22.
+//
+
+import SwiftUI
+
+struct ValidatingAndDisablingForms: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ValidatingAndDisablingForms_Previews: PreviewProvider {
+    static var previews: some View {
+        ValidatingAndDisablingForms()
+    }
+}
